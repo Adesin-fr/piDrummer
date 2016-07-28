03bffdf94ea50f71d15b1739297b9ab3f5c69ceb
Ludovic <l.lemarinel@gmail.com>
2016-07-28 15:33:51 +0200

    Added TextLabel Class Redesigned serial port handling to set blocking event of 10ms in each loop. The blocking event exits if data is not available, or if data is there ! This drastically lower CPU usage when waiting for data. Added key event (F12) to exit software with a return value=255. So oPi should handle it and poweroff the board. Removed references to GlobalSettings and ScreenSurface in classes (objects are accessed as global variable !

---
+++ TextLabel.cpp
@@ -0,0 +1,97 @@
+/*
+ * DrawLabel.cpp
+ *
+ *  Created on: 28 juil. 2016
+ *      Author: ulle0009
+ */
+
+#include "TextLabel.h"
+
+using namespace std;
+
+TextLabel::TextLabel() {
+	m_TextAlignment=TextLabel::LEFT_ALIGN;
+	m_xPos=0;
+	m_yPos=0;
+	m_maxWidth=2000;
+	m_fontSize=18;
+	m_drawingSurface=screen;
+	m_textColor=(SDL_Color){255,255,255};
+	m_textSelected=false;
+	fontLoaded=false;
+	myFont=NULL;
+}
+
+TextLabel::TextLabel(std::string text, unsigned int xPos, unsigned int yPos){
+	m_text=text;
+	m_TextAlignment=TextLabel::LEFT_ALIGN;
+	m_xPos=xPos;
+	m_yPos=yPos;
+	m_maxWidth=2000;
+	m_fontSize=18;
+	m_drawingSurface=screen;
+	m_textColor=(SDL_Color){255,255,255};
+	m_textSelected=false;
+	fontLoaded=false;
+	myFont=NULL;
+}
+
+void TextLabel::doDraw(){
+	SDL_Rect position;
+	SDL_Color invertColor;
+
+	string fontPath = myglobalSettings.getUserDirectory() + "/.urDrummer/res/arial.ttf";
+
+	if ( fontLoaded==false ){
+		myFont = TTF_OpenFont(fontPath.c_str(), m_fontSize);
+	}
+
+	SDL_Surface *texte ;
+
+	texte = TTF_RenderText_Blended(myFont, m_text.c_str(), m_textColor);
+
+	//calculate text position :
+
+	position.y=m_yPos;
+	switch(m_TextAlignment){
+		case TextLabel::LEFT_ALIGN:
+			position.x=m_xPos;
+			break;
+		case TextLabel::CENTER_ALIGN:
+			position.x=m_xPos-(texte->w/2);
+			break;
+		case TextLabel::RIGHT_ALIGN:
+			position.x=m_xPos-texte->w;
+			break;
+	}
+
+	// Delete the generated text : it will be re-generated !
+	delete texte;
+
+	if (m_textSelected){
+
+		invertColor.b=255-m_textColor.b;
+		invertColor.r=255-m_textColor.r;
+		invertColor.g=255-m_textColor.g;
+
+		texte = TTF_RenderText_Blended(myFont, m_text.c_str(), invertColor);
+
+		SDL_Surface *BGSurf;
+		BGSurf=SDL_CreateRGBSurface(SDL_HWSURFACE,texte->w, texte->h, 16,0,0,0,0);
+		SDL_FillRect(BGSurf, NULL, SDL_MapRGB(BGSurf->format, m_textColor.r, m_textColor.g, m_textColor.b));
+		SDL_BlitSurface(BGSurf, NULL, screen, &position); /* Blit background*/
+		delete BGSurf;
+	}else{
+		texte = TTF_RenderText_Blended(myFont, m_text.c_str(), m_textColor);
+	}
+
+	SDL_BlitSurface(texte, NULL, screen, &position); /* Blit text */
+
+	delete texte;
+
+}
+
+
+TextLabel::~TextLabel() {
+}
+
---
+++ TextLabel.h
@@ -0,0 +1,119 @@
+/*
+ * DrawLabel.h
+ *
+ *  Created on: 28 juil. 2016
+ *      Author: ulle0009
+ */
+
+#ifndef TEXTLABEL_H_
+#define TEXTLABEL_H_
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <string>
+#include "Settings.h"
+
+extern Settings myglobalSettings;
+extern SDL_Surface* screen;
+
+
+class TextLabel {
+
+	public:
+
+		enum TextAlign{
+			LEFT_ALIGN,
+			CENTER_ALIGN,
+			RIGHT_ALIGN
+		};
+
+		TextLabel();
+		TextLabel(std::string text, unsigned int xPos, unsigned int yPos);
+		void doDraw();
+		virtual ~TextLabel();
+
+	unsigned int getFontSize() const {
+		return m_fontSize;
+	}
+
+	void setFontSize(unsigned int fontSize) {
+		m_fontSize = fontSize;
+	}
+
+	unsigned int getMaxWidth() const {
+		return m_maxWidth;
+	}
+
+	void setMaxWidth(unsigned int maxWidth) {
+		m_maxWidth = maxWidth;
+	}
+
+	const std::string& getText() const {
+		return m_text;
+	}
+
+	void setText(const std::string& text) {
+		m_text = text;
+	}
+
+	TextLabel::TextAlign getTextAlignment() const {
+		return m_TextAlignment;
+	}
+
+	void setTextAlignment(TextLabel::TextAlign textAlignment) {
+		m_TextAlignment = textAlignment;
+	}
+
+	SDL_Color getTextColor() const {
+		return m_textColor;
+	}
+
+	void setTextColor(SDL_Color textColor) {
+		m_textColor = textColor;
+	}
+
+	bool isTextSelected() const {
+		return m_textSelected;
+	}
+
+	void setTextSelected(bool textSelected) {
+		m_textSelected = textSelected;
+	}
+
+	int getXPos() const {
+		return m_xPos;
+	}
+
+	void setXPos(int xPos) {
+		m_xPos = xPos;
+	}
+
+	int getYPos() const {
+		return m_yPos;
+	}
+
+	void setYPos(int yPos) {
+		m_yPos = yPos;
+	}
+
+	void setDrawingSurface(SDL_Surface *drawingSurface) {
+		m_drawingSurface = drawingSurface;
+	}
+
+	private:
+		std::string m_text;
+		unsigned int m_fontSize;
+		int m_xPos;
+		int m_yPos;
+		unsigned int m_maxWidth;
+		TextLabel::TextAlign m_TextAlignment;
+		SDL_Color m_textColor;
+		bool m_textSelected;
+		SDL_Surface *m_drawingSurface;
+
+		TTF_Font *myFont;
+		bool fontLoaded;
+
+};
+
+#endif /* TEXTLABEL_H_ */
