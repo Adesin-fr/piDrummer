40d05d2146383d76e6699e6d36ce75f445a88f0c
LemarinelNet <l.lemarinel@gmail.com>
2017-12-15 09:23:03 +0100

    Added splash files and conversion tool.

---
+++ convertRAWtoC.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+	$pass=0;
+	$cnt=0;
+	$ficContents=file_get_contents("splash.raw");
+
+	$size=filesize("splash.raw");
+
+	echo "unsigned char bitmapData[$size]={";
+
+	for ($i=0; $i<$size; $i++){
+		if ($pass==1){
+				echo ", ";
+		}
+
+		echo ord(substr($ficContents,$i,1));
+		$pass=1;
+		$cnt++;
+		if ($cnt==16){
+			echo "\n";
+			$cnt=0;
+		}
+	}
+
+	echo "};\n";
+
+
+?>
